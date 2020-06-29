b9b67b924b2075aa18399ab993cf22ed403fc377
Elena Casillas <elena.casillas@itexico.com>
2020-06-29 17:30:43 -0500

    Updates

---
+++ precache-manifest.2a9697c50dc8671a9aad608daa3ecdfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7690b15a3f54eff3d5ddad91e5e9986d",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "1c5563fb7847fdf74a0f",
+    "url": "/testapp/static/css/main.3609990e.chunk.css"
+  },
+  {
+    "revision": "4f6a1b343212e546e9f5",
+    "url": "/testapp/static/js/2.9dc68857.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testapp/static/js/2.9dc68857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5563fb7847fdf74a0f",
+    "url": "/testapp/static/js/main.c46b5698.chunk.js"
+  },
+  {
+    "revision": "ef401c629b0fb120d3eb",
+    "url": "/testapp/static/js/runtime-main.bd52d88b.js"
+  }
+]);
